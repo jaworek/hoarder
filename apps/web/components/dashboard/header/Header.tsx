f2ea6b16690c1dc340fbcedcb72c8cce8d35c3f5
Jan Jaworski <jaworek3211@gmail.com>
2025-04-03 23:08:35 +0200

    hide top bar elements on pages other than bookmarks

--- apps/web/components/dashboard/header/Header.tsx
+++ apps/web/components/dashboard/header/Header.tsx
@@ -1,16 +1,14 @@
+"use client";
+
 import Link from "next/link";
-import { redirect } from "next/navigation";
+import { usePathname } from "next/navigation";
 import GlobalActions from "@/components/dashboard/GlobalActions";
 import ProfileOptions from "@/components/dashboard/header/ProfileOptions";
 import { SearchInput } from "@/components/dashboard/search/SearchInput";
 import HoarderLogo from "@/components/HoarderIcon";
-import { getServerAuthSession } from "@/server/auth";
 
-export default async function Header() {
-  const session = await getServerAuthSession();
-  if (!session) {
-    redirect("/");
-  }
+export default function Header() {
+  const pathname = usePathname();
 
   return (
     <header className="sticky left-0 right-0 top-0 z-50 flex h-16 items-center justify-between overflow-x-auto overflow-y-hidden bg-background p-4 shadow">
@@ -19,10 +17,12 @@ export default async function Header() {
           <HoarderLogo height={20} gap="8px" />
         </Link>
       </div>
-      <div className="flex flex-1 gap-2">
-        <SearchInput className="min-w-40 bg-muted" />
-        <GlobalActions />
-      </div>
+      {pathname === "/dashboard/bookmarks" ? (
+        <div className="flex flex-1 gap-2">
+          <SearchInput className="min-w-40 bg-muted" />
+          <GlobalActions />
+        </div>
+      ) : null}
       <div className="flex items-center">
         <ProfileOptions />
       </div>
